4fd2bcb8766c82c65ed860c200139fd67ccc6e89
elliottscott774 <123982857+elliottscott774@users.noreply.github.com>
2024-03-06 15:35:29 -0500

    created report_count function

--- prompt.py
+++ prompt.py
@@ -1,1 +1,9 @@
-def report_count(): return 1
+def report_count(token):
+    text = open("corpus.txt")
+    split_text = text.split()
+    count = 0
+    for word in split_text:
+        if word == token:
+            count += 1
+    
+    return count
